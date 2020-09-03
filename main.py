c62fdd9df0459414c8f58bf8dccb951ec1dfbb90
iroquoispliskin <41455185+iroquoispliskin@users.noreply.github.com>
2020-09-03 19:03:46 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -7,52 +7,57 @@ Created on Fri Aug 14 18:39:30 2020
 from scrapes import *
 from threading import Thread
 import os
+import glob
+import pandas as pd
 
 
 def ab():
     ab_b = americanbarbell_barbell_e.main()
     ab_p = americanbarbell_plates_e.main()
-    ab_b.to_csv('data\data.csv', 'a+')
-    ab_p.to_csv('data\data.csv', 'a+')
+    ab_b.to_csv('out1.csv')
+    ab_p.to_csv('out2.csv')
     
 def bos():
     bos_b = ballsofsteel_barbells_e.results
     bos_p = ballsofsteel_plates_e.results
-    bos_b.to_csv('data\data.csv', 'a+')
-    bos_p.to_csv('data\data.csv', 'a+')
+    bos_b.to_csv('out3.csv')
+    bos_p.to_csv('out4.csv')
 
 def rf():
     rf_b = repfitness_barbells.main()
     rf_p = repfitness_plates_rework.main()
-    rf_b.to_csv('data\data.csv', 'a+')
-    rf_p.to_csv('data\data.csv', 'a+')
+    rf_b.to_csv('out5.csv')
+    rf_p.to_csv('out6.csv')
 
 def r():
     r_b = rogue_barbells.main()
     r_p = rogue_plates.main()
-    r_b.to_csv('data\data.csv', 'a+')
-    r_p.to_csv('data\data.csv', 'a+')
+    r_b.to_csv('out7.csv')
+    r_p.to_csv('out8.csv')
 
 def tf():
     tf_b = titanfitness_barbells.main()
     tf_p = titanfitness_plates.main()
-    tf_b.to_csv('data\data.csv', 'a+')
-    tf_p.to_csv('data\data.csv', 'a+')
+    tf_b.to_csv('out9.csv')
+    tf_p.to_csv('out10.csv')
 
 def xmf():
     xmf_b = xmarkfitness_barbells.main()
     xmf_p = xmarkfitness_plates.main()
-    xmf_b.to_csv('data\data.csv', 'a+')
-    xmf_p.to_csv('data\out12.csv', "a+")
+    xmf_b.to_csv('out11.csv')
+    xmf_p.to_csv('out12.csv')
     
 if __name__ == '__main__':
-    if not os.path.exists('data'):
-        os.makedirs('data')
-    Thread(target = ab).start()
-    Thread(target = bos).start()
-    Thread(target = rf).start()
-    Thread(target = r).start()
-    Thread(target = tf).start()
-    Thread(target = xmf).start()
+    threads = [Thread(target=t) for t in (ab, bos, rf, r, tf, xmf)]
+    os.chdir("data")
+    for thread in threads:
+        thread.start()
+    extension = 'csv'
+    all_filenames = [i for i in glob.glob('*.{}'.format(extension))]
+    for thread in threads:
+        thread.join()
+    combined_csv = pd.concat([pd.read_csv(f) for f in all_filenames ])
+    combined_csv.to_csv( "combined_csv.csv", index=False, encoding='utf-8-sig')
+    
     
     
